27940514e7f8d61b889e6822016d4f07d4257fd0
Amune Israel <iharmuneh@gmail.com>
2024-02-15 04:43:50 +0100

    new changes

--- ui/utils/getGoogleUrl.ts
+++ ui/utils/getGoogleUrl.ts
@@ -2,7 +2,7 @@ function getGoogleOAuthURL() {
     const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
     const options = {
       redirect_uri: 'http://localhost:1337/api/sessions/oauth/google',
-      client_id: '1091996600549-q6nkvaa86lt5smh4gp4jqssbourdvk5v.apps.googleusercontent.com',
+      client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as unknown as string,
       access_type: "offline",
       response_type: "code",
       prompt: "consent",
